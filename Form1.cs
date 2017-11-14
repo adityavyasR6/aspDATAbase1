43bdcc3b32be1c072656b5c61400d92849e165d0
adityavyasR6 <33662365+adityavyasR6@users.noreply.github.com>
2017-11-14 20:34:49 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ADITYA
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=G:\CIA PROJECT\TEST.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into data1 values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "')";
+            cmd.ExecuteNonQuery();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            con.Close();
+            disp_data();
+            MessageBox.Show("inserted successfully");
+        }
+        public void disp_data()
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from data1";
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            disp_data();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from data1 where name='" + textBox1.Text + "'";
+            cmd.ExecuteNonQuery();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            con.Close();
+            disp_data();
+            MessageBox.Show("deleted successfully");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update data1 set name='" + textBox2.Text + "' where name='" + textBox1.Text + "'";
+            cmd.ExecuteNonQuery();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            con.Close();
+            disp_data();
+            MessageBox.Show("updated successfully successfully");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from data1 where name='"+textBox1.Text+"'";
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+    }
+}
+    
+
